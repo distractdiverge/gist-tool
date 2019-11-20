0df2148a2b882aa9031ce38a417f13a307682204
Alex Lapinski <alapinski@paypal.com>
2019-11-20 16:30:46 -0500

    Added backend api

---
+++ packages/back/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { 
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  // TODO: Allow listening on 0.0.0.0 instead of just 127.0.0.1 for docker
+  await app.listen(3000);
+}
+
+// TODO: Trigger launch of bootstrap only if this file is run directly
+bootstrap();
